b21383aec3deaab3d5511c22ca182482e67e76a3
Kunal Kathpal <kkunal2103@outlook.com>
2023-04-09 20:52:47 +0530

    Home Page UI ready

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ session_start();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>HMS, CRSSIET, Jhajjar</title>
-    <link href="css/home.scss" rel="stylesheet">
+    <link rel="stylesheet" href="css/home.scss">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
