6912252b1b20f44f55bf075ebf7c99c5cf87e2da
Kunal Kathpal <kkunal2103@outlook.com>
2023-04-13 10:30:04 +0530

    made some structural changes

---
+++ aboutme.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"/>
+        <link rel="stylesheet" href="css\aboutme.scss">
+        <title>Contact us</title>
+    </head>
+    <body>
+        <div class="heading" >
+                <h1>Our Team</h1>
+        </div>
+
+        <div class="container">
+            <div class="card">
+                <div class="content">
+                    <div class="imgBx">
+                        <img src="assets/kunal.png" alt="Kunal Kathpal">
+                    </div>
+                    <div class="contentBx">
+                        <h4>Kunal Kathpal</h4>
+                        <h5>Student</h5>
+                    </div>
+                    <div class="sci">
+                        <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                        <a href="#"><i class="fa fa-brands fa-github" aria-hidden="true"></i></a>
+                        <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="content">
+                    <div class="imgBx">
+                        <img src="assets/kunal.png" alt="Adarsh Tiwari">
+                    </div>
+                    <div class="contentBx">
+                        <h4>Adarsh Tiwari</h4>
+                        <h5>Student</h5>
+                    </div>
+                    <div class="sci">
+                        <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                        <a href="#"><i class="fa fa-brands fa-github" aria-hidden="true"></i></a>
+                        <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+            </div>
+    </div>
+</body>
+</html>
