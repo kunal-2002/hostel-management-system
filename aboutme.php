b7aaae0c21254fcefab83e95e20e549f8b3d4b48
Kunal Kathpal <kkunal2103@outlook.com>
2023-04-13 11:10:36 +0530

    some changes done 13 April

--- aboutme.php
+++ aboutme.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"/>
-        <link rel="stylesheet" href="css\aboutme.scss">
+        <link rel="stylesheet" href="css\aboutme.css">
         <title>Contact us</title>
     </head>
     <body>
@@ -17,32 +17,32 @@
             <div class="card">
                 <div class="content">
                     <div class="imgBx">
-                        <img src="assets/kunal.png" alt="Kunal Kathpal">
+                        <img src="assets/kunal.jpg" alt="Kunal Kathpal">
                     </div>
                     <div class="contentBx">
                         <h4>Kunal Kathpal</h4>
                         <h5>Student</h5>
                     </div>
                     <div class="sci">
-                        <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                        <a href="#"><i class="fa fa-brands fa-github" aria-hidden="true"></i></a>
-                        <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                        <a href="https://www.linkedin.com/in/kunal-kathpal"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                        <a href="https://github.com/kunal-2002"><i class="fa fa-brands fa-github" aria-hidden="true"></i></a>
+                        <a href="https://www.instagram.com/_kunal._.kathpal"><i class="fa fa-instagram" aria-hidden="true"></i></a>
                     </div>
                 </div>
             </div>
             <div class="card">
                 <div class="content">
                     <div class="imgBx">
-                        <img src="assets/kunal.png" alt="Adarsh Tiwari">
+                        <img src="assets/adarsh.jpg" alt="Adarsh Tiwari">
                     </div>
                     <div class="contentBx">
                         <h4>Adarsh Tiwari</h4>
                         <h5>Student</h5>
                     </div>
                     <div class="sci">
-                        <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                        <a href="#"><i class="fa fa-brands fa-github" aria-hidden="true"></i></a>
-                        <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                        <a href="https://www.linkedin.com/in/adarshtiwari121003"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                        <a href="https://github.com/Adarsh121003"><i class="fa fa-brands fa-github" aria-hidden="true"></i></a>
+                        <a href="https://www.instagram.com/adarsh121003"><i class="fa fa-instagram" aria-hidden="true"></i></a>
                     </div>
                 </div>
             </div>
