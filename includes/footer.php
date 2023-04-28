8c15ab32df36e41beace6fbcd6ce1d74f14f35bc
Kunal Kathpal <kkunal2103@outlook.com>
2023-04-28 19:26:02 +0530

    Unwanted files removed

--- includes/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="footer">
-    <div class="page-container footer-container">
-        <div class="footer-copyright">Made with ❤️ by <b>HMS Team</b></div>
-    </div>
-</footer>
-
-<script type="text/javascript" src="js/jquery.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
