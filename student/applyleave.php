b6232007bc291d3af6f7be68f7afdcfa6169625c
Kunal Kathpal <kkunal2103@outlook.com>
2023-04-30 23:44:13 +0530

    Leave Application feature added

---
+++ student/applyleave.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Apply Leave</title>
+    <link rel="stylesheet" href="../css/applyleave.css">
+</head>
+    <script type="text/javascript" >
+        function goback(){
+            window.location.href = "studentdashboard.php";
+        }
+    </script>
+    <?php
+        session_start();
+    ?>
+    <?php
+        $regno = $_SESSION['regno'];
+        require_once('../dbConnect.php');
+        $sql = "SELECT name,block, roomno FROM users WHERE regno='$regno';";
+        $query1 = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_assoc($query1);
+        $name = $row['name'];
+        $block = $row['block'];
+        $roomno = $row['roomno'];
+        $errmsg="";
+        $sucmsg="";
+
+        // $result = mysqli_query($conn, "SELECT count($regno) as total from leaverequests where regno='regno' AND status='Pending';");
+        // $data = mysqli_fetch_assoc($result);
+        // $count = $data['total'];
+    ?>
+    <?php
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            if( isset($_POST['fromdate']) && isset($_POST['todate']) && isset($_POST['reason']) ){
+                $fromdate = $_POST['fromdate'];
+                $todate = $_POST['todate'];
+                $reason = $_POST['reason'];
+                date_default_timezone_set('Asia/Kolkata');
+                $date = date('d-m-y');
+                list($pday, $pmonth, $pyear) = explode('-', $date);
+                list($year1, $month1, $day1) = explode('-', $fromdate);
+                list($year2, $month2, $day2) = explode('-', $todate);
+                
+                $sql = "SELECT * FROM leaverequests WHERE regno='$regno' AND status='Pending'";
+                if($result = mysqli_query($conn, $sql)){
+                    $count = mysqli_num_rows($result);
+                }
+                // $month2<$month1 || $day2<$day1 || $day1<$pday || $day2<$pday || $month1<$pmonth || $month2<$pmonth
+                if($month2<$month1 || $day2<$day1 || $day1<$pday || $day2<$pday || $month1<$pmonth || $month2<$pmonth){
+                    $errmsg = "*Invalid dates: You entered wrong information";
+                }
+                else{
+                    if($count>=1){
+                        $errmsg = "*You already had a leave";
+                    }
+                    else{
+                        require_once('../dbConnect.php');
+                        $sql = "INSERT INTO `leaverequests` (`name`,`regno`,`block`,`roomno`,`fromdate`,`todate`,`reason`,`status`) VALUES ('$name','$regno','$block','$roomno','$fromdate','$todate','$reason','Pending')";
+                        $query = mysqli_query($conn, $sql);
+                        if($query){
+                            $sucmsg = '*Entry successful';
+                        }
+                        else{
+                            $errmsg = "*Error occured";
+                        }
+                    }
+                }
+            }
+        }
+    ?>
+<body>
+    <div class="container">
+        <div class="title">Leave Application</div>
+        <div class="content">
+            <form action="applyleave.php" method="post">
+                <div class="user-details">
+                    <div class="input-box">
+                        <span class="details">Full Name</span>
+                        <input type="text" placeholder="Enter your name" pattern="[a-z A-Z]*" value="<?php echo $name;  ?>" required disabled>
+                    </div>
+                    <div class="input-box">
+                        <span class="details">Reg No</span>
+                        <!-- pattern="[0-9]{2}[A-Z]{3}[0-9]{4}" -->
+                        <input type="text" placeholder="Enter your regno" value="<?php echo $regno; ?>" disabled required>
+                    </div>
+                    <div class="input-box">
+                        <span class="details">Block Name</span>
+                        <input type="text" placeholder="Enter your block name" value="<?php echo $block; ?>" disabled required>
+                    </div>
+                    <div class="input-box">
+                        <span class="details">Room no</span>
+                        <input type="number" placeholder="Enter your room " value="<?php echo $roomno; ?>" disabled required>
+                    </div>
+                    <div class="input-box">
+                        <span class="details">From</span>
+                        <input type="date" name="fromdate" id="fromdate" placeholder="Enter from date" required>
+                    </div>
+                    <div class="input-box">
+                        <span class="details">To</span>
+                        <input type="date" name="todate" id="todate" placeholder="Enter to date"  required>
+                    </div>
+                    <div class="input-box">
+                        <span class="details">Reason</span>
+                        <input type="textarea" name="reason" id="reason" placeholder="Reason"  required>
+                    </div>
+                </div>
+
+                <div class="button">
+                    <input type="button" value="Go Back" onclick="goback()">
+                    <input type="submit" name="submit" style="margin-left:85px;" >
+                </div>
+            </form>
+            <span style="color: red;"><?php echo $errmsg; ?></span>
+            <span style="color: green;"><?php echo $sucmsg; ?></span>
+        </div>
+    </div>
+</body>
+</html>
